4c96d70502e89749f3174b5a104ff6c31871d03d
Tatsiana Kukharchuk <tatsi-ana@hotmail.com>
2016-01-14 21:02:10 +0100

    tests! ready finaly!!!^:

--- spec/features/customer_has_orderlist_spec.rb
+++ spec/features/customer_has_orderlist_spec.rb
@@ -8,15 +8,13 @@ describe "test02", :type => :feature do
   it "show all orders to one customer" do
     visit "/customers/#{@customer.id}"
     @orders = Customer.where(name: "Dagobert").first.orders
-      #@orders.each do |order|
-	      #expect(page).to have_content order.placed_on + " " + order.status
-	    #end
+    expect(all("tr.customerOrder").count).to eq 6
 	end
 
   it "include Price in the Orders" do
     visit "/customers/#{@customer.id}"
     @filterdOrders = Order.where(:customer_id => @customer.id)
     totalSum = @filterdOrders.sum("price")
-    #expect(page).to have_content "Total Price: " + "#{totalSum}"
+    expect(page).to have_content "Total Price: " + "#{totalSum}"
   end
 end
